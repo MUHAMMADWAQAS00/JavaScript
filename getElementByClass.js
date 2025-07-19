ffe3b7a0dd442de53853d67e25e398542650ac4c
MUHAMMADWAQAS00 <mw2101607@gmail.com>
2025-07-19 15:35:23 +0500

    JavaScript Basics

---
+++ getElementByClass.js
@@ -0,0 +1,8 @@
+let element2 = document.getElementsByClassName('programming')
+console.log(element2);
+
+// // element2.innerHTML = "waqas"
+// Ye work nhii kre ga b/c array ha tou hm loop use kren gey
+for(let i=0;i < element2.length;i++){
+    element2[i].innerHTML = 'waah kiya bt ha'
+}
---
+++ getElementById.js
@@ -0,0 +1,9 @@
+
+let element = document.getElementById('wiki');
+console.log(element);
+console.log(element.innerHTML);
+element.innerHTML = "Hello Waqas You're doing Good Keep work hard";
+let element1 = document.getElementById('changes')
+console.log(element1.length)
+element1.innerHTML = 'My name Good Name';
+console.log("Hello World")
