7ee3b0b99c874a0db14883680de920bcaaa6199a
MUHAMMADWAQAS00 <mw2101607@gmail.com>
2025-07-29 12:33:07 +0500

    PracQuest for concept

---
+++ PQofJS/script.js
@@ -0,0 +1,4 @@
+//creating new claass in CSS trying to append in para(p)
+//whenever we want to add class with previous one we use classList
+let myclass = document.querySelector("p")
+myclass.classList.add("newClass")
