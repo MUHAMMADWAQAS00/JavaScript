4c10c02dfb59bb9a652adf365965fe85f3c53efd
MUHAMMADWAQAS00 <mw2101607@gmail.com>
2025-08-03 13:03:20 +0500

    preventdefaultinJS

--- PQofJS/script.js
+++ PQofJS/script.js
@@ -2,3 +2,9 @@
 //whenever we want to add class with previous one we use classList
 let myclass = document.querySelector("p")
 myclass.classList.add("newClass")
+myclass.classList.toggle("dark-mode");
+let btn = document.querySelector("button")
+btn.addEventListener("click", function() {
+  alert("Your are on a right way keep it up wiki!");
+
+});
---
+++ preventDefaultinJS/script.js
@@ -0,0 +1,9 @@
+let a = document.querySelector("#myform")
+
+a.addEventListener("submit",function(e){
+e.preventDefault();
+
+})
+a.addEventListener("submit",function(){
+    a.innerHTML = "waqas"
+})
