ae64521f6c6617e44ec2de5efb00f9a60bcb78ed
MUHAMMADWAQAS00 <mw2101607@gmail.com>
2025-08-06 13:19:07 +0500

    nothing to change

---
+++ inputeventsinJS/script.js
@@ -0,0 +1,21 @@
+let element = document.getElementById('me')
+
+
+function myFocus(){
+    element.style.backgroundColor = 'aqua'
+}
+function myBlur(){
+element.style.backgroundColor = "white"
+}
+// function myChange(){
+// console.log(this.value)
+// }
+function myInput(){
+// console.log(this.value)
+
+}
+element.addEventListener('focus',myFocus)
+element.addEventListener('blur',myBlur)
+//element.addEventListener('change',myChange)
+element.addEventListener('input',myInput)
+
